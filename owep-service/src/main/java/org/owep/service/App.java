9cb8da780bdf3f4e3a10c9f3bd0d160bc3bbcdc4
frankdou <frankdou0423@126.com>
2017-04-05 11:09:18 +0800

    adasd

---
+++ owep-service/src/main/java/org/owep/service/App.java
@@ -0,0 +1,13 @@
+package org.owep.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
